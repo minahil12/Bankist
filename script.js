519aaca5e6114281fdd69442ab6d6983743f1629
minahil12 <minahilmaroof5@gmail.com>
2025-01-25 20:30:23 +0500

    tabs and fading

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.sections');
+const allButtons = document.getElementsByTagName('button');
+const section1 = document.querySelector('#section--1');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,10 +39,79 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const header = document.querySelector('header');
-const allSections = document.querySelectorAll('.sections');
-const allButtons = document.getElementsByTagName('button');
+btnScrollTo.addEventListener('click', function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
+//Page Navigation
+//adding event handleer by foreach
+//but this will slow down the application
+/*
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+*/
+//Event delegation
+//adding event handler to parent
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//Tab Opeartion
+tabContainer.addEventListener('click', function (tab) {
+  const clicked = tab.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  //Remove active classes from all
+  tabs.forEach(tb => tb.classList.remove('operations__tab--active'));
+
+  tabContent.forEach(con =>
+    con.classList.remove('operations__content--active')
+  );
+  //add the active class to desired tab
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+const fading = function (e) {
+  //this keyword here refers to the argument passed
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+//passing argumetns into handler
+nav.addEventListener('mouseover', fading.bind(0.5));
+
+nav.addEventListener('mouseout', fading.bind(1));
+
+// sticky nav
+
+const oberver = new IntersectionObserver();
+oberver.observe();
+//Other work
+/*
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 
@@ -53,4 +131,36 @@ document
     message.remove();
   });
 
-document.documentElement.style.setProperty('--color-primary', 'purple');
+//document.documentElement.style.setProperty('--color-primary', 'purple');
+
+//clicking on the learn more button and scrolling to section 1
+
+//Events listners
+const h1 = document.querySelector('h1');
+
+const h1Alert = function () {
+  alert('Great! your hovered over h1');
+
+  h1.removeEventListener('mouseenter', h1Alert);
+};
+//h1.addEventListener('mouseenter', h1Alert);
+
+//Event Propogation
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  console.log(randomColor());
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+*/
